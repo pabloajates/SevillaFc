78beb07a41343fbbf9c35a8d2de197fc3d66aa6f
9.449 <pablo.ajasan@educa.jcyl.es>
2023-02-27 20:41:44 +0100

    Creadas clases y herencia

--- src/com/pajates/sevillaFC/domain/models/Entrenador.java
+++ src/com/pajates/sevillaFC/domain/models/Entrenador.java
@@ -3,4 +3,12 @@ package com.pajates.sevillaFC.domain.models;
 public class Entrenador extends Persona{
 
     private String estrategia;
+
+    public String getEstrategia() {
+        return estrategia;
+    }
+
+    public void setEstrategia(String estrategia) {
+        this.estrategia = estrategia;
+    }
 }
--- src/com/pajates/sevillaFC/domain/models/Medico.java
+++ src/com/pajates/sevillaFC/domain/models/Medico.java
@@ -3,4 +3,12 @@ package com.pajates.sevillaFC.domain.models;
 public class Medico extends Persona{
 
     private String especialidad;
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
 }
